0889b2320e20156d3c7a8f407dc350bf763887e2
RanaZainPNY <rzi.codealigned@gmail.com>
2025-01-11 14:06:44 +0500

    added keto template

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,43 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/dashboard', function () {
-    return view("mainPage");
+    // return view('welcome');
+    return view('index');
 });
 
+Route::get('/aboutPage', function () {
+    // return view('welcome');
+    return view('about');
+});
+
+Route::get('/contactPage', function () {
+    // return view('welcome');
+    return view('contact');
+});
+
+
+Route::get('/galleryPage', function () {
+    // return view('welcome');
+    return view('gallery');
+});
+
+Route::get('/blogPage', function () {
+    // return view('welcome');
+    return view('blog');
+});
+
+Route::get('/roomPage', function () {
+    // return view('welcome');
+    return view('room');
+});
+
+Route::get('/aboutPage', function () {
+    // return view('welcome');
+    return view('about');
+});
+
+Route::get('/roomPage', function () {
+    // return view('welcome');
+    return view('room');
+});
 
